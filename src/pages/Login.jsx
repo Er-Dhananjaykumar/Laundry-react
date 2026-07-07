b1ca6753927539d3549f9e75c20d1b0e820832ef
Dhananjay Kumar <dhananjaykr921@gmail.com>
2026-07-07 13:23:34 +0530

    Create professional login page UI

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,75 @@
-function Login(){
-    return(
-        <>
-        <h1>Login Page</h1>
-        <p>Login here.</p>
-        </>
-        
+import { useState } from "react";
+import "../styles/login.css";
+
+function Login() {
+
+    const [email, setEmail] = useState("");
+
+    const [password, setPassword] = useState("");
+
+    function handleSubmit(e) {
+
+        e.preventDefault();
+
+        console.log(email);
+
+        console.log(password);
+
+    }
+
+    return (
+
+        <div className="login-container">
+
+            <form
+                className="login-form"
+                onSubmit={handleSubmit}
+            >
+
+                <h1>Laundry Management</h1>
+
+                <p>Welcome Back</p>
+
+                <label>Email</label>
+
+                <input
+
+                    type="email"
+
+                    placeholder="Enter Email"
+
+                    value={email}
+
+                    onChange={(e) => setEmail(e.target.value)}
+
+                />
+
+                <label>Password</label>
+
+                <input
+
+                    type="password"
+
+                    placeholder="Enter Password"
+
+                    value={password}
+
+                    onChange={(e) => setPassword(e.target.value)}
+
+                />
+
+                <button type="submit">
+
+                    Login
+
+                </button>
+
+            </form>
+
+        </div>
+
     );
+
 }
 
 export default Login;
