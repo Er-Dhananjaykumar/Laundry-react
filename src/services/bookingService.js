db54e85cd52beaaffe3b6a3604cb709d656b0db0
Er-Dhananjaykumar <dhananjaykr921@gmail.com>
2026-08-08 10:40:38 +0530

    Integrated React application with backend service layer

--- src/services/bookingService.js
+++ src/services/bookingService.js
@@ -1,1 +1,26 @@
-export {};
+import api from "./api";
+
+const BOOKING_BASE_URL = import.meta.env.VITE_BOOKING_API_URL || "/bookings";
+
+export const bookingService = {
+  getAll: async () => {
+    const response = await api.get(BOOKING_BASE_URL);
+    return response.data;
+  },
+  getById: async (id) => {
+    const response = await api.get(`${BOOKING_BASE_URL}/${id}`);
+    return response.data;
+  },
+  create: async (booking) => {
+    const response = await api.post(BOOKING_BASE_URL, booking);
+    return response.data;
+  },
+  update: async (id, booking) => {
+    const response = await api.put(`${BOOKING_BASE_URL}/${id}`, booking);
+    return response.data;
+  },
+  remove: async (id) => {
+    const response = await api.delete(`${BOOKING_BASE_URL}/${id}`);
+    return response.data;
+  }
+};
--- src/services/customerService.js
+++ src/services/customerService.js
@@ -1,1 +1,26 @@
-export {};
+import api from "./api";
+
+const CUSTOMER_BASE_URL = import.meta.env.VITE_CUSTOMER_API_URL || "/customers";
+
+export const customerService = {
+  getAll: async () => {
+    const response = await api.get(CUSTOMER_BASE_URL);
+    return response.data;
+  },
+  getById: async (id) => {
+    const response = await api.get(`${CUSTOMER_BASE_URL}/${id}`);
+    return response.data;
+  },
+  create: async (customer) => {
+    const response = await api.post(CUSTOMER_BASE_URL, customer);
+    return response.data;
+  },
+  update: async (id, customer) => {
+    const response = await api.put(`${CUSTOMER_BASE_URL}/${id}`, customer);
+    return response.data;
+  },
+  remove: async (id) => {
+    const response = await api.delete(`${CUSTOMER_BASE_URL}/${id}`);
+    return response.data;
+  }
+};
